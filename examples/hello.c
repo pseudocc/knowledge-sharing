eff70edafbde5a244c90f564ffa9797de62e83d2
pseudoc <atlas.yu@canonical.com>
2023-03-13 11:07:47 +0800

    showcases

---
+++ examples/hello.c
@@ -0,0 +1,8 @@
+const char title[] = "Knowledge sharing";
+
+const char motions_be_good[] = "Vim motions are probably useful.";
+
+const char lorem_sentence[] = "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.";
+const char lorem_paragraph[] = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.";
+
+const int zeroes[50] = { 0 };
